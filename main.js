96c04d4742ddb483cbf309528dcb1873af3b366d
CertIanS <irsnider13@gmail.com>
2026-08-20 13:58:48 -0400

    Fixed mobile menu and underlines

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var menu = document.getElementById("mobileMenu");
 
 function openMenu(){
     menu.classList.add("show");
+    menu.classList.remove("hide");
 }
 
 function closeMenu(){
     menu.classList.remove("show");
+    menu.classList.add("hide");
 }
